94370d6f8a4cc70fd8fbee46e0a59e29cead9aad
Md Abdulla Al Mamun <almamunjhpi@gmmail.com>
2022-12-26 23:23:34 +0600

    https://pub.dev/packages/liquid_swipe

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  final pages = [
+    Container(color: Colors.blue,),
+    Container(color: Colors.green,),
+    Container(color: Colors.orange,),
+    Container(color: Colors.yellow,),
+    Container(color: Colors.black,),
+    Container(color: Colors.red,),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: LiquidSwipe(pages: pages,),
+      ),
+
+    );
+  }
+}
+
+
